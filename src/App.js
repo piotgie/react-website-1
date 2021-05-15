b37ec72643b554a82818c7fbc34a20cb432a879e
Piotr Gaik <piotgie@gmail.com>
2021-05-15 17:39:50 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Navbar from './components/pages/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/HomePage/Home'
+import Footer from './components/pages/Footer/Footer'
+import Services from './components/pages/Services/Services'
+import Products from './components/pages/Products/Products'
+import SignUp from './components/pages/SignUp/SignUp'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" exact component={Services} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/signup" exact component={SignUp} />
+      </Switch>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
